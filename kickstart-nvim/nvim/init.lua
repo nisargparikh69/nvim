561cdc6dd9fb26251db45d59f5bde51b0a78bfd3
Nisarg Parikh <nisargparikh69@gmail.com>
2024-08-14 11:03:51 +0530

    removed priority to speedup early launch of nvim

--- kickstart-nvim/nvim/init.lua
+++ kickstart-nvim/nvim/init.lua
@@ -760,8 +760,6 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'rebelot/kanagawa.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
      require('kanagawa').setup({
       })
